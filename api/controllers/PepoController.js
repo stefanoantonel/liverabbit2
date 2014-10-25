27e293e25dfb1513ffd3365a141e6e82d6cb9689
stefanoantonel <stefanoantonel@gmail.com>
2014-10-25 00:10:57 -0300

    cambios

--- api/controllers/PepoController.js
+++ api/controllers/PepoController.js
@@ -1,13 +1,11 @@
 /**
- * AlumnoController
+ * PepoController
  *
- * @description :: Server-side logic for managing alumnoes
+ * @description :: Server-side logic for managing pepoes
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
 module.exports = {
-	fggfdgfd
-	ffsfsf
 	
 };
 
---
+++ api/models/Pepo.js
@@ -0,0 +1,17 @@
+/**
+* Pepo.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+  	username  : { type: 'string', unique: true },
+    email     : { type: 'email',  unique: true },
+    id : {type:"integer"}
+
+  }
+};
+
